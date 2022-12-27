67ec958b491278d4a29a97ad69bff2e22f1bf66e
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2022-12-26 20:08:30 -0500

    add design img, and adjust the css for the hover in dropdown

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -36,10 +36,14 @@ export default {
   <OnClickOutside @trigger="onClickOutside">  
     <div
       class="dropdown__container"
+      :class="{ 'dropdown--open': isOpen }"
       @click="onClickInside"
     >
       <div class="dropdown__question-container">
-        <span class="dropdown__question">{{ question }}</span>
+        <span
+          class="dropdown__question"
+          
+        >{{ question }}</span>
         <div class="dropdown__icon-container">
           <img src="../assets/icon-arrow-down.svg" alt="Icon" class="dropdown__icon" />
         </div>
@@ -61,11 +65,33 @@ export default {
 .dropdown {
   &__container {
     padding: 20px 0px;
+    line-height: 1.8em;
+
+    &:hover {
+      cursor: pointer;
+
+      &:not(.dropdown--open) .dropdown__question {
+        color: constants.$soft-red;
+      }
+    }
   }
   
   &__question-container {
     display: flex;
     justify-content: space-between;
+  }
+
+  &__answer-container {
+    margin-top: 10px;
+    color: constants.$dark-grayish-blue;
+  }
+
+  &--open {
+
+    & .dropdown__question {
+      font-weight: 700;
+      color: constants.$very-dark-desaturated-blue;
+    }
   }
 }
 </style>
